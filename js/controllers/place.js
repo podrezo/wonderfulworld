2548ce1d8cde176811268c0b0e9444daccf009d3
Petro Podrezo <petro@podrezo.com>
2016-02-24 18:19:40 -0500

    Made a naive implementation of similarty index calculation

--- js/controllers/place.js
+++ js/controllers/place.js
@@ -5,7 +5,35 @@
     function($scope, $stateParams, $location, $state, db) {
       $scope.place = _.findWhere(db, {id:$stateParams.id});
       if($scope.place) {
-        // do something maybe?
+        // map entire DB into array containing similarity values
+        var similarities = _.map(db, function(cmp) {
+          var similarityIndex = 0;
+          if(cmp.id === $stateParams.id) {
+            // do not count similarities between a place and itself
+            similarityIndex = -1;
+          } else {
+            for(var i in cmp.tags) {
+              for(var j in $scope.place.tags) {
+                if(cmp.tags[i] === $scope.place.tags[j]) {
+                  similarityIndex++;
+                }
+              }
+            }
+          }
+          return {
+            value: cmp,
+            similarityIndex: similarityIndex
+          };
+        });
+        similarities = _.sortBy(similarities, 'similarityIndex');
+        $scope.similarPlaces = [];
+        for(var i = 0; i < 4; i++) {
+          var currentPlace = similarities[similarities.length-1-i];
+          if(currentPlace.similarityIndex < 1) {
+            break;
+          }
+          $scope.similarPlaces.push(currentPlace.value);
+        }
       } else {
         $scope.place = {
           id: $stateParams.id,
