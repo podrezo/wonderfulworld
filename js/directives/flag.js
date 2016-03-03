6f6b69e8e0cbb030798562829a6c55913418af16
Petro Podrezo <petro@podrezo.com>
2016-03-03 11:21:45 -0500

    Turned all flag images into a spritesheet for optimization

--- js/directives/flag.js
+++ js/directives/flag.js
@@ -8,16 +8,16 @@
         countryCode: '=countryCode',
         size: '=size'
       },
-      /*link: function(scope, element, attrs) {
-        scope.size = scope.size || 16;
+      link: function(scope, element, attrs) {
         var setFlagImage = function() {
-          scope.imgUrl = scope.countryCode ? '/wonderfulworld/img/flags/'+scope.size+'/' + scope.countryCode + '.png' : '/wonderfulworld/img/flags/'+scope.size+'/_United Nations.png'
+          var countryCode = scope.countryCode || 'un';
+          var size = scope.size || '16';
+          attrs.$set('class', 'flag flag-' + size + '-' + countryCode.toLowerCase());
         };
-        scope.$watch('countryCode', setFlagImage);
         setFlagImage();
-      },
-      template: '<img ng-src="{{imgUrl}}" alt="{{countryCode}}" class="flag"/>'*/
-      template: '<img ng-src="/wonderfulworld/img/flags/{{size || \'16\'}}/{{countryCode || \'_United Nations\'}}.png" alt="{{countryCode}}" class="flag"/>'
+        scope.$watch('countryCode', setFlagImage);
+      }
+      //template: '<img ng-src="/wonderfulworld/img/flags/{{size || \'16\'}}/{{countryCode || \'_United Nations\'}}.png" alt="{{countryCode}}" class="flag"/>'
     };
   });
 })();
